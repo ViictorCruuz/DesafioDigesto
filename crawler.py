e0c143ca3c09524052ebc9062225b8ae1b8fdb1a
Victor Cruz <victor.victor2012@hotmail.com>
2020-03-11 10:25:21 -0300

    Functions that extract and save datas from specific sites

---
+++ crawler.py
@@ -0,0 +1,35 @@
+from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup
+
+
+
+def vultr_web_page():
+    req_vultr = Request('https://www.vultr.com/products/cloud-compute/#vc2', headers={'User-Agent': 'Chrome/18.0.1025.133'})
+    
+    vultr_page = urlopen(req_vultr).read()
+
+    soup = BeautifulSoup(vultr_page, 'html.parser')
+
+    return soup.select('strong')
+
+
+def digitalocean_web_page():
+    req_digital = Request('https://www.digitalocean.com/pricing/', headers={'User-Agent': 'Chrome/18.0.1025.133'})
+
+    ocean_page = urlopen(req_digital).read()
+
+    soup = BeautifulSoup(ocean_page, 'html.parser')
+
+    return soup.select('strong')
+
+
+vultr = vultr_web_page()
+
+ocean = digitalocean_web_page()
+
+with open('crawler.txt', 'w') as file:
+    for vr in vultr[:-7]:
+        file.write(vr.text + '\n')
+
+    for dt in ocean:
+        file.write(dt.text + '\n')
